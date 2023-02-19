1fb6c6141ba25167d0ec0a6db3fe7a8aafa09e60
Lucas Bueno <lucas-50p@hotmail.com>
2023-02-19 15:48:59 -0300

    First controller and Razor pages test

--- SalesWebCourse/Controllers/HomeController.cs
+++ SalesWebCourse/Controllers/HomeController.cs
@@ -11,17 +11,26 @@ namespace SalesWebCourse.Controllers {
     public class HomeController : Controller {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger) {
-            _logger = logger;
-        }
+            public HomeController(ILogger<HomeController> logger) {
+                _logger = logger;
+            }
 
-        public IActionResult Index() {
-            return View();
-        }
+            public IActionResult Index() {
+                return View();
+            }
 
-        public IActionResult Privacy() {
-            return View();
-        }
+            public IActionResult About() {
+
+                ViewData["Message"] = "Web MVC";
+                ViewData["name"] = "lucas";
+
+                return View();
+            }
+
+            public IActionResult Privacy() {
+
+                return View();
+            }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() {
