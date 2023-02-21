1242f8b5d463f744ebff8961081525e35a29ddad
Lucas Bueno <lucas-50p@hotmail.com>
2023-02-20 21:20:36 -0300

    CRUD Scaffolding

---
+++ SalesWebCourse/Data/SalesWebCourseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebCourse.Models.ViewModels;
+
+namespace SalesWebCourse.Data
+{
+    public class SalesWebCourseContext : DbContext
+    {
+        public SalesWebCourseContext (DbContextOptions<SalesWebCourseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebCourse.Models.ViewModels.Department> Department { get; set; }
+    }
+}
