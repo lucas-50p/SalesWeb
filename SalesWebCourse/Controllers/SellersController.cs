1b1a614773e129149f8d80746a6630890748003b
Lucas Bueno <lucas-50p@hotmail.com>
2023-03-22 20:41:46 -0300

    SellersController

---
+++ SalesWebCourse/Controllers/SellersController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebCourse.Controllers {
+    public class SellersController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
